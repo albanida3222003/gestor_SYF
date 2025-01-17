e6c102d5b7bb04ac18c79ecdf31763b1ba0b3839
bill <hopercalderon@gmail.com>
2025-01-17 18:32:06 -0500

    1.10 documentacion

--- usuarios_practica/src/main/java/com/unu/controllers/ProveedoresController.java
+++ usuarios_practica/src/main/java/com/unu/controllers/ProveedoresController.java
@@ -6,35 +6,60 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
-/**
- * Servlet implementation class ProveedoresController
- */
+import com.unu.beans.Consumidor;
+import com.unu.beans.Proveedores;
+import com.unu.model.ConsumidorModel;
+import com.unu.model.ProveedoresModel;
+
 public class ProveedoresController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+	private ProveedoresModel proveedoresModel = new ProveedoresModel();
+	
+	
     public ProveedoresController() {
         super();
         // TODO Auto-generated constructor stub
     }
+    
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+		String operacion = request.getParameter("operacion");
+
+		try {
+			if (operacion == null) {
+				listar(request, response);
+			} else {
+				switch (operacion) {
+				case "listar":
+					listar(request, response);
+					break;
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("processRequest() " + e.getMessage());
+		}
+	}
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		processRequest(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		processRequest(request, response);
 	}
-
+	
+	protected void listar(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			List<Proveedores> proveedores = proveedoresModel.listarProveedores();
+			request.setAttribute("proveedores", proveedores);
+			request.getRequestDispatcher("/proveedores/listarProveedores.jsp").forward(request, response);
+		} catch (Exception e) {
+			System.out.println("listar() " + e.getMessage());
+		}
+	}
 }
